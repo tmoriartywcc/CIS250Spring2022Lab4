ba27a71667b20ffcc7c84a2a6308e7ca6e380573
Tim Moriarty <timmoriarty@gmail.com>
2022-02-10 00:02:21 -0600

    initial files

---
+++ StatePair.java
@@ -0,0 +1,9 @@
+public class StatePair <Type1 extends Comparable<Type1>, Type2 extends Comparable<Type2>> {
+   private Type1 value1;
+   private Type2 value2;
+   
+   // TODO: Define a constructor, mutators, and accessors 
+   //       for StatePair
+   
+   // TODO: Define printInfo() method
+}
